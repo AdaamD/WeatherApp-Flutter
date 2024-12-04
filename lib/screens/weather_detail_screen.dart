469cb08714d5c4fbbb100eaab45d9530d8e41aa9
Adam <adam22daia11@gmail.com>
2024-12-04 10:18:57 +0100

    Ajout : Prévisions ur 5 jours

--- lib/screens/weather_detail_screen.dart
+++ lib/screens/weather_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../models/weather_model.dart';
+import 'package:intl/intl.dart';
 
 class WeatherDetailScreen extends StatelessWidget {
   final WeatherModel weather;
@@ -11,7 +12,7 @@ class WeatherDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Détails Météo'), // Titre de l'AppBar
+        title: Text('Météo pour ${weather.cityName}'),
       ),
       body: Container(
         decoration: BoxDecoration(
@@ -20,53 +21,103 @@ class WeatherDetailScreen extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  weather.cityName,
-                  style: GoogleFonts.lato(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
+        child: Column(
+          children: [
+            Flexible(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        weather.cityName,
+                        style: GoogleFonts.lato(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        DateFormat('dd/MM/yyyy - HH:mm')
+                            .format(DateTime.parse(weather.date)),
+                        style: GoogleFonts.lato(
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        '${weather.temperature} °C',
+                        style: GoogleFonts.lato(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'Précipitation : ${weather.precipitation} mm',
+                        style: GoogleFonts.lato(
+                          fontSize: 18,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'Humidité : ${weather.humidity} %',
+                        style: GoogleFonts.lato(
+                          fontSize: 18,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Text(
-                  weather.date,
-                  style: GoogleFonts.lato(
-                    fontSize: 20,
-                    color: Colors.black,
-                  ),
-                ),
-                SizedBox(height: 20),
-                Text(
-                  '${weather.temperature} °C',
-                  style: GoogleFonts.lato(
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-                Text(
-                  'Précipitation : ${weather.precipitation} mm',
-                  style: GoogleFonts.lato(
-                    fontSize: 18,
-                    color: Colors.black,
-                  ),
-                ),
-                Text(
-                  'Humidité : ${weather.humidity} %',
-                  style: GoogleFonts.lato(
-                    fontSize: 18,
-                    color: Colors.black,
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
+            Container(
+              height: 150, // Augmentation de la hauteur
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: weather.forecasts.length,
+                itemBuilder: (context, index) {
+                  final forecast = weather.forecasts[index];
+                  return Card(
+                    child: Container(
+                      width: 120, // Augmentation de la largeur
+                      padding: EdgeInsets.all(8),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            '${forecast.date.day}/${forecast.date.month}',
+                            style: GoogleFonts.lato(fontSize: 16),
+                          ),
+                          Image.network(
+                            'http://openweathermap.org/img/w/${forecast.icon}.png',
+                            height: 50,
+                            width: 50,
+                          ),
+                          Text(
+                            '${forecast.temperature.round()}°C',
+                            style: GoogleFonts.lato(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            forecast.description,
+                            style: GoogleFonts.lato(fontSize: 12),
+                            textAlign: TextAlign.center,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,16 +1,23 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import '../config.dart'; // Importez le fichier de configuration
+import '../config.dart';
 import '../models/weather_model.dart';
 
 class WeatherService {
   Future<WeatherModel> fetchWeather(String city) async {
-    final response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$OPENWEATHER_API_KEY&units=metric'));
+    try {
+      final response = await http.get(Uri.parse(
+          'https://api.openweathermap.org/data/2.5/forecast?q=$city&appid=$OPENWEATHER_API_KEY&units=metric'));
 
-    if (response.statusCode == 200) {
-      return WeatherModel.fromJson(json.decode(response.body));
-    } else {
+      if (response.statusCode == 200) {
+        final weatherData = json.decode(response.body);
+        return WeatherModel.fromJson(weatherData);
+      } else {
+        throw Exception(
+            'Failed to load weather data: ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      print('Error fetching weather: $e');
       throw Exception('Failed to load weather data');
     }
   }
